180fb89f476968e2725db691cd849295f12fc831
Sam Reither <samsreither@gmail.com>
2025-02-13 08:58:16 -0500

    just starting the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+  
+
+  return (
+    <>
+      <MovieCard movie={{title: "Sam's Film", release_date: "2024"}}/>
+    </>
+  )
+}
+
+export default App
